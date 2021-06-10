9abc4c953c57aaf4906a27792e659f7bad9d7401
michael.babsek <michael.babsek@babsek.de>
2021-06-10 22:19:44 +0200

    code reformatting

--- main.py
+++ main.py
@@ -8,26 +8,26 @@ board = chess.Board()
 
 print(board)
 while not board.is_game_over():
-    if not board.is_game_over():
-        if board.turn:
 
-            playerMove = input()
-            board.push_san(playerMove)
-            timeStart = time.time()
-            print(board)
+    if board.turn:
 
-        elif not board.turn:
+        playerMove = input()
+        board.push_san(playerMove)
+        timeStart = time.time()
+        print(board)
 
-            print("-------------------------------")
-            AIMove = AI.findBestMoveNegaMax(board, board.legal_moves)
-            board.push(AIMove)
-            timeEnd = time.time()
-            print("Time to move: " + str(timeEnd - timeStart))
-            evaluation = AI.evaluate(board)
-            print("Evaluation " + str(evaluation))
-            print("AI Move: " + chess.Move.__str__(AIMove))
-            print(board)
-            print("-------------------------------")
+    elif not board.turn:
+
+        print("-------------------------------")
+        AIMove = AI.findBestMoveNegaMax(board, board.legal_moves)
+        board.push(AIMove)
+        timeEnd = time.time()
+        print("Time to move: " + str(timeEnd - timeStart))
+        evaluation = AI.evaluate(board)
+        print("Evaluation " + str(evaluation))
+        print("AI Move: " + chess.Move.__str__(AIMove))
+        print(board)
+        print("-------------------------------")
 
 else:
     print("Game Over: " + board.result())
